4bc09209ed4fad0c7dc2d2f93f8bff2caa5c0ec2
Diogo Alves <diogo.alves@gmail.com>
2025-07-26 10:46:34 -0300

    Add script to document commands for recreating source study model

---
+++ scripts/03-recreate-source-study-model.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Warning: The model weights used in our experiments are located in the trained_models directory."
+echo "This script is intended only to document the commands used in the first creation."
+echo .
+echo python baseline_fine_grained.py Classroom m1 Classroom m1 Classroom_m1_Classroom_m1_242.h5 242
+echo python baseline_fine_grained.py Classroom m2 Classroom m2 Classroom_m2_Classroom_m2_242.h5 242
+echo python baseline_fine_grained.py Classroom m3 Classroom m3 Classroom_m3_Classroom_m3_242.h5 242
+echo python baseline_fine_grained.py Office m1 Office m1 Office_m1_Office_m1_242.h5 242
+echo python baseline_fine_grained.py Office m2 Office m2 Office_m2_Office_m2_242.h5 242
+echo python baseline_fine_grained.py Office m3 Office m3 Office_m3_Office_m3_242.h5 242
+echo .
